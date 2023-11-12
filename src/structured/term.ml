333d8a3fc9f3a392d496311cba25a6fdca811493
Colin Siles <colin.siles16@gmail.com>
2023-11-12 12:56:43 -0700

    Correctly shift the type vars in the recursive context

--- src/structured/term.ml
+++ src/structured/term.ml
@@ -21,13 +21,26 @@ and shift_type_vars_base (amount : int) (base_type : base_type) =
 and shift_type_vars_func (amount : int) ((arg, return) : unary_function) =
   (shift_type_vars_union amount arg, shift_type_vars_union amount return)
 
+and shift_type_vars_context (amount : int) (context : recursive_context) =
+  List.map
+    (fun flat_union ->
+      List.map
+        (fun flat_base ->
+          match flat_base with
+          | FLabel _ -> flat_base
+          | FIntersection functions ->
+              FIntersection (List.map (shift_type_vars_func amount) functions))
+        flat_union)
+    context
+
 let get_type_in_context (t : structured_type)
     (recursive_context : recursive_context) : structured_type =
-  let new_recursive_context = recursive_context @ t.context in
+  let shift_amount = List.length recursive_context in
+  let new_context = shift_type_vars_context shift_amount t.context in
   let new_union =
     shift_type_vars_union (List.length recursive_context) t.union
   in
-  build_structured_type new_union new_recursive_context
+  build_structured_type new_union (recursive_context @ new_context)
 
 (** [type_lambda_term term] determines the type of a term, if it is well-typed *)
 let rec get_type (term : term) = get_type_rec term TypeContextMap.empty (-1) []
